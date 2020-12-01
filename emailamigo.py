5d99cf1c361ff49e34b30124a4a29fff6fc4f35d
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2020-11-30 23:58:42 -0300

    Add files via upload

--- emailamigo.py
+++ emailamigo.py
@@ -1,33 +1,35 @@
 import smtplib
-
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
+import time
 
-def enviarmail(toadrr, name, namedest):
+
+def enviarmail(*args):
     try:
+        atual = time.localtime()
         fromaddr = "jrfirmino01@gmail.com"
         # toaddr = 'julreifir@yahoo.com.br'
         msg = MIMEMultipart()
 
         msg['From'] = fromaddr
-        msg['To'] = toadrr
-        msg['Subject'] = 'Amigo Secreto 2020 (Família Firmino)'
+        msg['To'] = args[0]
+        msg['Subject'] = f'Amigo Secreto {atual[0]} (Família Firmino)'
 
-        body = "\nChegou o grande dia!!!.\nHoje você saberá o seu amigo oculto 2020 " \
-               f"(Grupo da Família Firmino),\nSegue descrito abaixo o nome do seu amigo oculto:" \
-               f"\n{name}, segue o nome do seu amigo secreto --> {namedest.upper()}.\nEspero que tenha gostado!!!!"
+        body = f"Como vai {args[1]}, tudo bem com você?.\nHoje é o grande dia, você conhecerá o seu " \
+               f"amigo oculto {atual[0]} - (Grupo da Família Firmino).\nSegue descrito abaixo " \
+               f"o nome do seu amigo oculto:\nPrepare-se pois, para a sua surpresa o nome do seu grande " \
+               f"amigo secreto é {args[2].upper()}.\nNão conta para ninguem e espero que tenha gostado!!!.\nParabens!!!"
 
         msg.attach(MIMEText(body, 'plain'))
 
         filename = 'Resume.pdf'
 
-        attachment = open(filename,'rb')
-
+        attachment = open(filename, 'rb')
 
         part = MIMEBase('application', 'octet-stream')
-        part.set_payload((attachment).read())
+        part.set_payload(attachment.read())
         encoders.encode_base64(part)
         part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
 
@@ -37,10 +39,10 @@ def enviarmail(toadrr, name, namedest):
 
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
-        server.login(fromaddr, '*******')
+        server.login(fromaddr, 'Manuela01*')
         text = msg.as_string()
-        server.sendmail(fromaddr, toadrr, text)
+        server.sendmail(fromaddr, args[0], text)
         server.quit()
-        print('\nEmail enviado ao destinatário com sucesso!')
+        print(f'Email enviado para {args[1]} com sucesso!\n')
     except:
         print("\nErro ao enviar email")
